25d52138218c61de8f4c0ad2d39df5ea036b313a
Martin WEB. Larsson <20martin.larsson@glimnet.se>
2022-01-13 10:43:15 +0100

    feat: skeleton for fetch added

---
+++ case-my-cinema/src/components/Fetch.js
@@ -0,0 +1,9 @@
+export default function Fetch() {
+    return (
+        // TO DO, fetch movies from! 
+        // https://www.tvmaze.com/api
+        <main>
+            <h1>Fetch me some (To console)</h1>
+        </main>
+    )
+}
